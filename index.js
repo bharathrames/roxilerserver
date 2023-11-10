01fb387ccbbb5ba7839ca37c2f19417cb0d11e01
Bharathkumar <bharath91505@gmail.com>
2023-11-10 15:46:28 +0530

    upd

--- index.js
+++ index.js
@@ -4,10 +4,10 @@ const cors = require('cors');
 require('dotenv').config();
 const app = express();
 const port = process.env.PORT;
-// app.use(cors({
-//   // origin:"http://localhost:3000"
-//   origin:"https://roxilerdashboard.netlify.app/"
-// }))
+app.use(cors({
+  // origin:"http://localhost:3000"
+  origin:"https://roxilerdashboard.netlify.app/"
+}))
 
 const allowedOrigins = ['https://roxilerdashboard.netlify.app/'];
 app.use(cors({
